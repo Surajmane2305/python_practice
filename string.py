556cb1fd1448a31ef0909aac6e8523a90987b273
Surajmane2305 <surajmane2305@gmail.com>
2025-12-02 10:34:40 +0530

    Reverse the string

--- string.py
+++ string.py
@@ -4,3 +4,10 @@ num = 12345678
 strList = list(map(int, str(num)))
 print("Num to List...", strList)
 
+
+#Revers String withut splice
+str = 'suraj'
+rev = ''
+for s in str:
+    rev = s + rev
+print("Reversed string..", rev)
